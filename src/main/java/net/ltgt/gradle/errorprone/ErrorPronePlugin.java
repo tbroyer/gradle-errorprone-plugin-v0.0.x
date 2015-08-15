5d13674a0a99b157486d6c9400e84df2d51eef24
Thomas Broyer <t.broyer@ltgt.net>
2015-08-15 17:21:04 +0200

    Update to Gradle 2.6

--- src/main/java/net/ltgt/gradle/errorprone/ErrorPronePlugin.java
+++ src/main/java/net/ltgt/gradle/errorprone/ErrorPronePlugin.java
@@ -7,15 +7,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskCollection;
 import org.gradle.api.tasks.compile.JavaCompile;
-import org.gradle.jvm.platform.JavaPlatform;
-import org.gradle.language.base.internal.compile.CompileSpec;
-import org.gradle.language.base.internal.compile.Compiler;
-import org.gradle.platform.base.Platform;
-import org.gradle.platform.base.internal.toolchain.DefaultResolvedCompiler;
-import org.gradle.platform.base.internal.toolchain.DefaultResolvedTool;
-import org.gradle.platform.base.internal.toolchain.ResolvedTool;
-import org.gradle.platform.base.internal.toolchain.ToolResolver;
-import org.gradle.platform.base.internal.toolchain.ToolSearchResult;
 
 public class ErrorPronePlugin implements Plugin<Project> {
   @Override
@@ -26,32 +17,7 @@ public class ErrorPronePlugin implements Plugin<Project> {
     final Action<JavaCompile> action = new Action<JavaCompile>() {
       @Override
       public void execute(JavaCompile task) {
-        final ToolResolver oldToolResolver = task.getToolResolver();
-        task.setToolResolver(new ToolResolver() {
-          @Override
-          public <P extends Platform> ToolSearchResult checkToolAvailability(P requirement) {
-            if (requirement instanceof JavaPlatform) {
-              return toolChain.select((JavaPlatform) requirement);
-            }
-            return oldToolResolver.checkToolAvailability(requirement);
-          }
-
-          @Override
-          public <T, P extends Platform> ResolvedTool<T> resolve(Class<T> toolType, P requirement) {
-            if (requirement instanceof JavaPlatform) {
-              return new DefaultResolvedTool<T>(toolChain.select((JavaPlatform) requirement), toolType);
-            }
-            return oldToolResolver.resolve(toolType, requirement);
-          }
-
-          @Override
-          public <C extends CompileSpec, P extends Platform> ResolvedTool<Compiler<C>> resolveCompiler(Class<C> specType, P requirement) {
-            if (requirement instanceof JavaPlatform) {
-              return new DefaultResolvedCompiler<C>(toolChain.select((JavaPlatform) requirement), specType);
-            }
-            return oldToolResolver.resolveCompiler(specType, requirement);
-          }
-        });
+        task.setToolChain(toolChain);
       }
     };
 
