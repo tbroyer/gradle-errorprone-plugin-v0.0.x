c59f8439ab4a867eb6fb0a740ff70f69c506040c
Thomas Broyer <t.broyer@ltgt.net>
2015-08-16 02:51:43 +0200

    Fix tests
    All this time we weren't actually testing Error-prone but just JavaC.

--- integrationTests/failure/src/main/java/test/Failure.java
+++ integrationTests/failure/src/main/java/test/Failure.java
@@ -1,8 +1,8 @@
 package test;
 
 public class Failure {
-  public void method(int a) {
-    // Copied from https://code.google.com/p/error-prone/wiki/DivZero
-    double y = (double) a / 0.0
+  // See http://errorprone.info/bugpattern/ArrayEquals
+  public boolean arrayEquals(int[] a, int[] b) {
+    return a.equals(b);
   }
 }
--- integrationTests/success/src/main/java/test/Success.java
+++ integrationTests/success/src/main/java/test/Success.java
@@ -1,4 +1,9 @@
 package test;
 
 public class Success {
+  // See http://errorprone.info/bugpattern/ArrayEquals
+  @SuppressWarnings("ArrayEquals")
+  public boolean arrayEquals(int[] a, int[] b) {
+    return a.equals(b);
+  }
 }
