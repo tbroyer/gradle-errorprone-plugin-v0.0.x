524dcd399e80038d375b0d96a5b3d31254142344
Thomas Broyer <t.broyer@ltgt.net>
2016-08-06 03:07:06 +0200

    Update to Gradle 3.0-rc-1

--- src/test/groovy/net/ltgt/gradle/errorprone/IntegrationTestHelper.groovy
+++ src/test/groovy/net/ltgt/gradle/errorprone/IntegrationTestHelper.groovy
@@ -1,6 +1,6 @@
 package net.ltgt.gradle.errorprone
 
 class IntegrationTestHelper {
-    static final GRADLE_VERSIONS = System.getProperty("test.gradle-versions", "2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14")
+    static final GRADLE_VERSIONS = System.getProperty("test.gradle-versions", "2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,3.0-rc-1")
             .tokenize(',');
 }
