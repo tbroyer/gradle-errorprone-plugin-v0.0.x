fc577574a5b99f77c7b5873bead2f17eab58ccf3
Thomas Broyer <t.broyer@ltgt.net>
2017-09-22 10:18:19 +0200

    Update to Gradle 4.2

--- src/test/groovy/net/ltgt/gradle/errorprone/IntegrationTestHelper.groovy
+++ src/test/groovy/net/ltgt/gradle/errorprone/IntegrationTestHelper.groovy
@@ -1,6 +1,6 @@
 package net.ltgt.gradle.errorprone
 
 class IntegrationTestHelper {
-    static final GRADLE_VERSIONS = System.getProperty("test.gradle-versions", "2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,3.0,3.1,3.2,3.3,3.4,3.5,4.0")
+    static final GRADLE_VERSIONS = System.getProperty("test.gradle-versions", "2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,3.0,3.1,3.2,3.3,3.4,3.5,4.0,4.1,4.2")
             .tokenize(',');
 }
