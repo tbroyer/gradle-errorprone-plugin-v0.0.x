af069135985a8eeeb1ca9e2a16366fb66417ad55
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2018-04-25 16:27:01 -0700

    Use explicit imports.

--- src/main/java/net/ltgt/gradle/errorprone/ErrorProneCompiler.java
+++ src/main/java/net/ltgt/gradle/errorprone/ErrorProneCompiler.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.gradle.api.artifacts.Configuration;
